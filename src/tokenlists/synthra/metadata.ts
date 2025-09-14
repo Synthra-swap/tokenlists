5ae1d6b17cb15f1c8dba3457f4e5346deed8e3b3
jacko06v <jacomosconi@gmail.com>
2025-09-14 20:31:55 +0200

    migrated to turing uomi

--- src/tokenlists/synthra/metadata.ts
+++ src/tokenlists/synthra/metadata.ts
@@ -1,6 +1,6 @@
 export const metadata = {
   name: 'Synthra',
-  version: { major: 0, minor: 0, patch: 1 },
+  version: { major: 4, minor: 0, patch: 1 },
   keywords: [],
   logoURI:
     'https://pbs.twimg.com/profile_images/1901553077242798080/HLyntoRr_400x400.jpg',
--- src/tokenlists/synthra/overwrites.ts
+++ src/tokenlists/synthra/overwrites.ts
@@ -3,17 +3,17 @@ import { Network, OverwritesForList } from '../../types'
 
 export const overwrites: OverwritesForList = {
   [Network.Uomi]: {
-    '0xAA9C4829415BCe70c434b7349b628017C59EC2b1': {
+    '0x911b4000D3422F482F4062a913885f7b035382Df': {
       logoURI:
         'https://assets-cdn.trustwallet.com/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png',
     }, //USDC
-    '0x5FCa78E132dF589c1c799F906dC867124a2567b2': {
+    '0x1b8FA17f94DB8481E17152AaB8D242d438180BCf': {
       logoURI:
         'https://pbs.twimg.com/profile_images/1803434790680506368/tEAb8qfo_400x400.jpg',
     }, //WUOMI
-    '0x2922B2Ca5EB6b02fc5E1EBE57Fc1972eBB99F7e0': {
+    '0x5B727Bf35dDba2A8DA966E29f7c769823e4Faf41': {
       logoURI:
-        'https://raw.githubusercontent.com/Synthra-swap/tokenlists/refs/heads/main/src/assets/images/tokens/0x2922B2Ca5EB6b02fc5E1EBE57Fc1972eBB99F7e0.png',
+        'https://raw.githubusercontent.com/Synthra-swap/tokenlists/refs/heads/main/src/assets/images/tokens/0x5B727Bf35dDba2A8DA966E29f7c769823e4Faf41.png',
     }, //Synthra
     '0x04B03e3859A25040E373cC9E8806d79596D70686': {
       logoURI:
